00d13e1ab56642ec4f6e09102fd4db36385b0bc4
Pin <pin@Lu-MacPro.local>
2015-10-09 11:12:54 -0400

    fixed error

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express   = require('express'),
-    PORT      = process.env.PORT || 5432;
-    server    = express ();
+    PORT      = process.env.PORT || 5432,
+    server    = express (),
     MONGOURI  = process.env.MONGOLAB_URI || "monogdb://localhost:27017",
     dbname    ="some_usef_name",
-    mongoose  = require('mongoose');
+    mongoose  = require("mongoose");
+
 
     server.get('/super-secret-test', function (req, res) {
       res.write("welcome to my amazing app");
