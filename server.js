64ea338baa98333177367903786991b8b9d4d4e5
Pin <pin@Lu-MacPro.local>
2015-10-09 11:24:16 -0400

    updated file

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express   = require('express'),
     PORT      = process.env.PORT || 5432,
-    server    = express (),
+    server    = express(),
     MONGOURI  = process.env.MONGOLAB_URI || "monogdb://localhost:27017",
-    dbname    ="some_usef_name",
+    dbname    ="db_test",
     mongoose  = require("mongoose");
 
 
     server.get('/super-secret-test', function (req, res) {
       res.write("welcome to my amazing app");
       res.end();
-
     });
+    
 mongoose.connect(MONGOURI + "/" + dbname);
     server.listen(PORT, function() {
       console.log("SERVER IS UP ON PORT:", PORT);
