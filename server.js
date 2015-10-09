614dcbed094790e1a858bcb3d1249c34909563e7
Pin <pin@Lu-MacPro.local>
2015-10-09 11:06:03 -0400

    updated

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express'),
-    PORT = process.env.PORT || 5432;
-    server = express ();
-    MONGOURI =process.env.MONGOLAB_URI || "monogdb://localhost:27017",
-    dbname ="some_usef_name",
-    mongoose = require('monogoose');
+var express   = require('express'),
+    PORT      = process.env.PORT || 5432;
+    server    = express ();
+    MONGOURI  = process.env.MONGOLAB_URI || "monogdb://localhost:27017",
+    dbname    ="some_usef_name",
+    mongoose  = require('mongoose');
 
     server.get('/super-secret-test', function (req, res) {
       res.write("welcome to my amazing app");
