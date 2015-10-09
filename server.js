502017bbe071a78fa48dbf31358353c45cff7c1d
Pin <pin@Lu-MacPro.local>
2015-10-09 10:39:48 -0400

    added mongo_url stuff

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     PORT = process.env.PORT || 5432;
     server = express ();
+    MONGOURI =process.env.MONGOLAB_URI,
+    dbname ="some_usef_name",
+    mongoose = require('monogoose');
 
     server.get('/super-secret-test', function (req, res) {
       res.write("welcome to my amazing app");
       res.end();
 
     });
-
+mongoose.connect(MONGOURI + "/" + dbname);
     server.listen(PORT, function() {
       console.log("SERVER IS UP ON PORT:", PORT);
     });
